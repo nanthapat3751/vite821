6766bc97c6b1a789ac1bde2b8bcf996c9bbd0807
nanthapat3751 <nanthapat.l@ku.th>
2025-03-08 16:56:42 +0700

    Exp Edit

---
+++ src/contents/experience.js
@@ -0,0 +1,43 @@
+import { faGithub, faMedium, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import picExp from '../assets/picExp1.png'
+import picExp2 from '../assets/picExp2.png'
+ 
+export const data = [
+    {
+        date: "2020-2021",
+        title: "Fullstack · Sport Analysis System",
+        link: "https://nanthapat3751.github.io/vite820/",
+        materials: [
+            { type: faGithub, link: "https://nanthapat3751.github.io/vite820/" },
+            { type: faYoutube, link: "https://nanthapat3751.github.io/vite820/" },
+            { type: faMedium, link: "https://nanthapat3751.github.io/vite820/" },
+        ],
+        descriptions: [
+            "Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.",
+            "Create tools for monitoring the backend and implementing self-recovery mechanisms, ensuring their seamless operation 24/7.",
+        ],
+        skills: [
+            ["React", "Redus", "MUI"],
+        ],
+        picture: picExp,
+       
+    },
+    {
+        date: "2020-2021",
+        title: "Backend Dev · Sport Analysis System",
+        link: "https://nanthapat3751.github.io/vite820/",
+        material: [
+            { type: faGithub, link: "https://nanthapat3751.github.io/vite820/" },
+            { type: faYoutube, link: "https://nanthapat3751.github.io/vite820/" },
+            { type: faMedium, link: "https://nanthapat3751.github.io/vite820/" },
+        ],
+        descriptions: [
+            "Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.",
+            "Create tools for monitoring the backend and implementing self-recovery mechanisms, ensuring their seamless operation 24/7.",
+        ],
+        skills: [
+            ["React", "Redus", "MUI"],
+        ],
+        picture: picExp2,
+    }
+]
